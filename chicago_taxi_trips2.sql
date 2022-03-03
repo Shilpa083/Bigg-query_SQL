32271360e100eb058bda50ae1e29e00bdd36f6db
Shilpa083 <100786231+Shilpa083@users.noreply.github.com>
2022-03-03 17:16:34 +0530

    Update chicago_taxi_trips2.sql

--- chicago_taxi_trips2.sql
+++ chicago_taxi_trips2.sql
@@ -1,3 +1,5 @@
+--Q2) From which Taxi Company maximum number of chicago taxis are there?
+
 SELECT company AS Taxi_company, COUNT(company) AS Frequency
 from `bigquery-public-data.chicago_taxi_trips.taxi_trips` 
 GROUP BY 1
